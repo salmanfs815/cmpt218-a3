d135c92bdfef7500852b99d13acf36ccde21227d
Salman Siddiqui <salmanf.siddiqui@gmail.com>
2018-03-07 22:58:56 -0800

    internal JSON used for data
    todo:
    - mongodb
    - style data tables
    - test

--- app.js
+++ app.js
@@ -1,84 +1,120 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 
-var app = express();
-var port = process.env.PORT || 23298;
+var app = express()
+var port = process.env.PORT || 23298
 
-var admin = {user: 'salman', pass: 'siddiqui'};
-var loggedIn = false;
-var sessionID = NaN;
+var admin = {user: 'salman', pass: 'siddiqui'}
+var loggedIn = false
+var checkinID = ''
+var sessionID = NaN
 
-var checkins = {};
+var checkins = {}
 
 app.use('/', (req, res, next) => {
-	console.log(req.method, 'request:', req.url);
-	next();
-});
+  console.log(req.method, 'request:', req.url)
+  next()
+})
 
-app.use(express.static('static'));
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.static('static'))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 app.get('/favicon.ico', (req, res) => {
-	res.sendFile(path.join(__dirname, 'static', 'favicon.ico'));
-});
+  res.sendFile(path.join(__dirname, 'static', 'favicon.ico'))
+})
+
+function prepareJSON (checkin) {
+  var res = []
+  for (var sess in checkins[checkin]) {
+    checkins[checkin][sess]['attendees'].forEach((user) => {
+      console.log('user', user)
+      res.push({
+        name: user.name,
+        id: user.id,
+        time: user.time,
+        session: sess
+      })
+    })
+  }
+  return res
+}
 
 app.post('/', (req, res) => {
-	if (req.body.user === admin.user &&
-		req.body.pass === admin.pass) {
-		loggedIn = true;
-		res.send('true');
-	} else {
-		loggedIn = false;
-		res.send('false');
-	}
-});
+  if (req.body.user === admin.user &&
+    req.body.pass === admin.pass) {
+    loggedIn = true
+    res.send('true')
+  } else {
+    loggedIn = false
+    res.send('false')
+  }
+})
 
 app.post('/logout', (req, res) => {
-	loggedIn = false;
-	sessionID = NaN;
-	res.sendStatus(200);
-});
+  loggedIn = false
+  sessionID = NaN
+  checkinID = ''
+  res.sendStatus(200)
+})
 
-app.post('/admin', (req,res) => {
-	if(!loggedIn) {
-		res.redirect('/');
-	} else {
-		sessionID = Date.now();
-		res.send(sessionID.toString());
-	}
-});
+app.post('/admin', (req, res) => {
+  if (!loggedIn) {
+    res.redirect(401, '/')
+  } else {
+    checkinID = req.body.checkinID
+    sessionID = Date.now()
+    var sessionObj = {
+      "admin": admin.user,
+      "start": Date(),
+      "end": null,
+      "attendees": [] 
+    }
+    if (!(checkinID in checkins)) {
+      checkins[checkinID] = {}
+    }
+    checkins[checkinID][sessionID] = sessionObj
+    res.send(sessionID.toString())
+  }
+})
 
 app.post('/history', (req, res) => {
-	if(!loggedIn) {
-		res.redirect('/');
-	} else {
-		checkinID = req.body.checkinID;
-		res.json(/* full checkin history */);
-	}
-});
+  if (!loggedIn) {
+    res.redirect(401, '/')
+  } else {
+    checkinID = req.body.checkinID
+    res.json(prepareJSON(checkinID))
+  }
+})
 
 app.post('/adminCheckin', (req, res) => {
-	if(!loggedIn) {
-		res.redirect('/');
-	} else {
-		checkinID = req.body.checkinID;
-		res.json(/* checkin history for current session */);
-		sessionID = NaN;
-	}
-});
+  if (!loggedIn) {
+    res.redirect(401, '/')
+  } else {
+    var checkin = req.body.checkinID
+    var session = req.body.sessionID
+    checkins[checkin][session]["end"] = Date()
+    res.json(checkins[checkin][session]['attendees'])
+    sessionID = NaN
+    checkinID = ''
+  }
+})
 
 app.post('/attendeeCheckin', (req, res) => {
-	var checkin = req.body.checkin;
-	// check if checkin is open
-	var attendee = {
-		name: req.body.name,
-		id:  req.body.id,
-		time: Date()
-	};
-	// add attendee to checkin
-	res.send(admin.user);
-});
+  var checkin = req.body.checkin
+  // check if checkin is open
+  if (checkinID === checkin) {
+      var attendee = {
+        name: req.body.name,
+        id: req.body.id,
+        time: Date()
+      }
+      checkins[checkinID][sessionID]["attendees"].push(attendee)
+      // add attendee to checkin
+      res.send(admin.user)
+  } else {
+    res.send('')
+  }
+})
 
-app.listen(port, () => console.log(`Server is listening on port ${port}`));
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
--- static/script.js
+++ static/script.js
@@ -1,97 +1,126 @@
 
 var app = new Vue({
-	el: '#app',
-	data: {
-		// available views: 'adminLogin', 'adminLanding', 'history',
-		//		'adminCheckin', 'adminDone', 'attendeeCheckin', 'attendeeDone'
-		view: 'adminLogin',
-		loginErr: false,
-		/*adminUser: '',
-		adminPass: '',
-		checkinID: '',
-		sessionID: '',
-		attendeeName: '',
-		attendeeID: '',
-		attendeeCheckinID: ''*/
-		adminUser: 'salman',
-		adminPass: 'siddiqui',
-		checkinID: 'CMPT218',
-		sessionID: '',
-		attendeeName: 'Bob Smith',
-		attendeeID: '123456789',
-		attendeeCheckinID: 'CMPT218'
-	},
-	methods: {
-		adminLogin() {
-			// axios post to '/': user = this.adminUser, pass = this.adminPass
-			axios.post('/', {
-				user: this.adminUser,
-				pass: this.adminPass
-			}).then((res) => {
-				console.log(res)
-				if(res.data == true) {
-					this.adminPass = '';
-					this.view = 'adminLanding';
-				} else {
-					this.loginErr = true;
-				}
-			}).catch((err) => {
-				console.log(err);
-			});
-		},
-		adminLogout() {
-			axios.post('/logout', {})
-			.catch((err) => {
-				console.log(err);
-			});
-			this.sessionID = '';
-			this.view = 'adminLogin';
-		},
-		adminCheckin() {
-			// axios post to '/admin': checkinID = this.checkinID
-			axios.post('/admin', {
-				checkinID: this.checkinID
-			}).then((res) => {
-				this.sessionID = res.data.toString();
-				console.log('Session ID:', this.sessionID);
-			}).catch((err) => {
-				console.log(err);
-			});
-			this.view = 'adminCheckin';
-		},
-		history() {
-			// axios post to '/history': checkinID = this.checkinID
-			axios.post('/history', {
-				checkinID = this.checkinID
-			}).then((res) => {
-				// res is JSON that needs to be displayed in tbody of view
-			}).catch((err) => {
-				console.log(err);
-			});
-			this.view = 'history';
-		},
-		adminEndCheckin() {
-			// axios post to '/adminCheckin': checkinID = this.checkinID
-			axios.post('/adminCheckin', {
-				checkinID = this.checkinID
-			}).then((res) => {
-				// res is JSON that needs to be displayed in tbody of view
-			});
-			this.view = 'adminDone';
-		},
-		attendeeCheckin() {
-			// axios post to '/attendeeCheckin': name = this.attendeeName,
-			//		id = this.attendeeID, checkin = this.attendeeCheckinID
-			axios.post('/attendeeCheckin', {
-				name: this.attendeeName,
-				id: this.attendeeID,
-				checkin: this.attendeeCheckinID
-			}).then((res) => {
-				this.adminUser = res;
-			}).catch((err) => {
-				console.log(err);
-			});
-			this.view = 'attendeeDone';
-		}
-	}
-});
+  el: '#app',
+  data: {
+    // available views: 'adminLogin', 'adminLanding', 'history',
+    //    'adminCheckin', 'adminDone', 'attendeeCheckin', 'attendeeDone'
+    view: 'adminLogin',
+    loginErr: false,
+    /*
+    adminUser: '',
+    adminPass: '',
+    checkinID: '',
+    sessionID: '',
+    attendeeName: '',
+    attendeeID: '',
+    attendeeCheckinID: '',
+    checkinHistory: [],
+    sessionHistory: []
+    */
+    adminUser: 'salman',
+    adminPass: 'siddiqui',
+    checkinID: 'CMPT218',
+    sessionID: '',
+    attendeeName: 'Bob Smith',
+    attendeeID: '123456789',
+    attendeeCheckinID: 'CMPT218',
+    checkinHistory: [],
+    sessionHistory: []
+  },
+  methods: {
+    adminLogin () {
+      // axios post to '/': user = this.adminUser, pass = this.adminPass
+      axios.post('/', {
+        user: this.adminUser,
+        pass: this.adminPass
+      }).then((res) => {
+        if (res.data === true) {
+          this.adminPass = ''
+          this.view = 'adminLanding'
+        } else {
+          this.loginErr = true
+        }
+      }).catch((err) => {
+        console.log(err)
+      })
+    },
+    adminLogout () {
+      axios.post('/logout', {})
+        .catch((err) => {
+          console.log(err)
+        })
+      this.sessionID = ''
+      this.view = 'adminLogin'
+    },
+    adminCheckin () {
+      // axios post to '/admin': checkinID = this.checkinID
+      axios.post('/admin', {
+        checkinID: this.checkinID
+      }).then((res) => {
+        this.sessionID = res.data.toString()
+        this.view = 'adminCheckin'
+      }).catch((err) => {
+        if (err.response.status === 401) {
+          alert('Please log in to continue.')
+          this.view = 'adminLogin'
+        }
+      })
+    },
+    history () {
+      // axios post to '/history': checkinID = this.checkinID
+      axios.post('/history', {
+        checkinID: this.checkinID
+      }).then((res) => {
+        // res is JSON that needs to be displayed in tbody of view
+        this.checkinHistory = res.data
+        this.view = 'history'
+      }).catch((err) => {
+        if (err.response && err.response.status === 401) {
+          alert('Please log in to continue.')
+          this.view = 'adminLogin'
+        } else {
+          console.log(err)
+        }
+      })
+    },
+    adminEndCheckin () {
+      // axios post to '/adminCheckin': checkinID = this.checkinID
+      axios.post('/adminCheckin', {
+        sessionID: this.sessionID,
+        checkinID: this.checkinID
+      }).then((res) => {
+        // res is JSON that needs to be displayed in tbody of view
+        this.sessionHistory = res.data
+        this.view = 'adminDone'
+      }).catch((err) => {
+        if (err.response && err.response.status === 401) {
+          alert('Please log in to continue.')
+          this.view = 'adminLogin'
+        } else {
+          console.log(err)
+        }
+      })
+    },
+    attendeeCheckin () {
+      // axios post to '/attendeeCheckin': name = this.attendeeName,
+      //    id = this.attendeeID, checkin = this.attendeeCheckinID
+      axios.post('/attendeeCheckin', {
+        name: this.attendeeName,
+        id: this.attendeeID,
+        checkin: this.attendeeCheckinID
+      }).then((res) => {
+        if (res.data === '') {
+          alert('Sorry, check in for '+ this.attendeeCheckinID + ' is currently closed.')
+        } else {
+          this.adminUser = res.data
+          this.view = 'attendeeDone'
+        }
+      }).catch((err) => {
+        console.log(err)
+      })
+    },
+    prepCheckinData (data) {
+
+    }
+  }
+})
